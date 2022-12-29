6dd3e59cde7f645f5269d40796c722398a5117e8
Billyngwa <billytatangwa@gmail.com>
2022-12-29 13:20:47 +0100

    added new changes to chat-pannel

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,3 +4,20 @@ messageFlag.forEach(flag =>{
         flag.style.display = 'none';
     }
 });
+
+const cross = document.querySelector('.cross');
+let chatPannel = document.querySelector('.chat-pannel');
+let displayProfile = document.querySelector('div#display-profile');
+let userProfile = document.querySelector('section.user-profile');
+let availableContact = document.querySelector('.available-chat');
+displayProfile.addEventListener('click', event => {
+    chatPannel.classList.add('view');
+    availableContact.classList.add('view');
+    userProfile.classList.add('view');
+});
+cross.addEventListener('click', event=>{
+    displayProfile.classList.remove('view');
+    userProfile.classList.remove('view');
+    availableContact.classList.remove('view');
+    chatPannel.classList.remove('view');
+});
